a0432319e1f448a6b045795437daa15d3cdce2d4
foram patel <p.foram147@gmail.com>
2021-07-08 23:31:13 +0100

    added li and dropdown

--- maketeam.js
+++ maketeam.js
@@ -1,8 +1,34 @@
-const makeTeamForm = document.getElementById("maketam-Form")
-const makeTeamBtn = document.getElementById("maketeam-Btn")
-//const numOfMem = document.getElementById("numof-mem")
-
-makeTeamBtn.addEventListener("click",(e) => {
-    e.preventDefault();
-    const makeTeam = makeTeamForm.makeTeam.value;
-    const teamMemNum = makeTeamForm.teamMemNum.value;})
+function makeTeam(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("addName").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("addName").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+  for(var i=2; i<=15 ; i++){
+      if(i%2===0){
+    var select = document.getElementById("tmMem");
+    var option = document.createElement("OPTION");
+	select.options.add(option);
+	option.text = i;
+	option.value = i;
+}
+}
